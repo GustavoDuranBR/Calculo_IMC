53a2ce7d5009722b3797716b414a4d4d94f9bfe3
Gustavo Duran <gustavoduran22@gmail.com>
2022-08-19 10:46:21 -0300

    Correção da lógica do cálculo Percentual de gordura e superfície corporal Ajustes da formatação na saída dos resultados

--- imc.py
+++ imc.py
@@ -21,7 +21,7 @@ layout = [
     [sg.Text('Digite sua IDADE', size=(14, 1)), sg.Input(size=(4, 1), key='idade')],
     [sg.Radio('Homem', 1, key='sexo'), sg.Radio('Mulher', 0, key='sexo')],
     [sg.Button('Calcular'), sg.Button('Limpar', key='clear'), sg.Button('Sair')],
-    [sg.Output(size=(52, 12))],
+    [sg.Output(size=(52, 14))],
     [sg.Text('Desenvolvedor: Gustavo Duran', size=(40, 0))]
 ]
 
@@ -56,27 +56,29 @@ while True:
         imc = valor_peso / altura_metros ** 2
         pg1 = ((1.20 * imc) + (0.23 * valor_idade) - (10.8 * valor_sexo) - 5.4)
         sc1 = (((valor_peso * 4) + 7) / (valor_peso + 90))
-        titulo(data_string)
+
         nome = f'{valor_nome}'
         peso = f'{valor_peso}'
         altura = f'{altura_metros}'
         idade = f'{valor_idade}'
         sc = f'{sc1:.1f}'
-        pg = f'{pg1:.2f}'
+        pg = f'{pg1:.1f}'
+        pg_teste = f'{int(pg1)}'
 
         peso_texto = str(peso).replace('.', ',')
         altura_texto = str(altura).replace('.', ',')
         sc_texto = str(sc).replace('.', ',')
         pg_texto = str(pg).replace('.', ',')
 
+        titulo(data_string)
         print(f' • {nome}')
         print(f' • Peso {peso_texto}kg')
         print(f' • Altura {altura_texto}')
         print(f' • Idade {idade} anos')
-        print(f' • IMC {imc:.1f}(kg/m²)')
         print(f' • Superfície Corporal {sc_texto}(m²)')
-        print(f' • Percentual de gordura {pg_texto}%')
+        print(f' • IMC {imc:.1f}(kg/m²)')
 
+        # IMC
         if imc < 16:
             print(f' • {nome}, você está abaixo do peso (grau I)')
         elif imc < 17:
@@ -94,5 +96,69 @@ while True:
         else:
             print(f' • {nome}, você está no grau III de Obesidade')
 
-janela.close()
+        print(f' • Percentual de gordura {pg_texto}%')
 
+        # Percentual de Gordura Corporal (Mulher)
+        if valor_sexo == 0:
+            if valor_idade <= 39:
+                if pg1 < 21:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 33:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 39:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+            elif valor_idade <= 59:
+                if pg1 < 23:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 35:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 40:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+            elif valor_idade <= 79:
+                if pg1 < 24:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 36:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 42:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+
+        # Percentual de Gordura Corporal (Homem)
+        elif valor_sexo == 1:
+            if valor_idade <= 39:
+                if pg1 < 8:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 20:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 25:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+            elif valor_idade <= 59:
+                if pg1 < 11:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 22:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 28:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+            elif valor_idade <= 79:
+                if pg1 < 13:
+                    print(f' • {nome}, você está com pouca gordura')
+                elif pg1 <= 25:
+                    print(f' • {nome}, você está saudável')
+                elif pg1 <= 30:
+                    print(f' • {nome}, você está acima do percentual de gordura')
+                else:
+                    print(f' • Cuidado {nome}, você está com Obesidade')
+        else:
+            print(' • Algo errado não está certo')
+
+
+janela.close()
